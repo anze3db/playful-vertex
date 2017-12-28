e8d8391d351bac7fd58405aef8ec80a8dbbedd44
Anže Pečar <anze@pecar.me>
2017-12-28 01:02:54 +0000

    Add tests for Index component

---
+++ frontend/test/unit/specs/components/Index.spec.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import { mount } from 'vue-test-utils'
+import Index from '@/components/Index'
+import store from '@/store'
+
+describe('Index.vue', () => {
+  it('should render correct contents', () => {
+    const wrapper = mount(Index, {store})
+    expect(wrapper.find('h1').text()).toEqual('Hello')
+  })
+})
