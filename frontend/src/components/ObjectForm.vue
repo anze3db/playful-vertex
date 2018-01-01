495bd84c7bec7b1d1881f8ddf667fcf109d1ff6f
Anže Pečar <anze@pecar.me>
2018-01-01 23:55:54 +0000

    Veutify ObjectForm

--- frontend/src/components/ObjectForm.vue
+++ frontend/src/components/ObjectForm.vue
@@ -1,10 +1,14 @@
 <template>
-  <form>
-    <input v-model="title" placeholder="Title..."></input><br>
-    <textarea v-model="description" placeholder="Description"></textarea>
-    <br>
-    <button v-on:click="submit">Submit</button>
-  </form>
+  <v-container grid-list-md text-xs-center>
+    <v-layout row wrap>
+      <v-flex xs12>
+        <v-text-field v-model="title" label="Title"></v-text-field><br>
+        <v-text-field multi-line v-model="description" label="Description"></v-text-field>
+        <br>
+        <v-btn color="info" v-on:click="submit" name="submit">Submit</v-btn>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
--- frontend/test/unit/specs/components/ObjectForm.spec.js
+++ frontend/test/unit/specs/components/ObjectForm.spec.js
@@ -6,16 +6,15 @@ import store from '@/store'
 describe('Form.vue', () => {
   it('should render correct contents', () => {
     const wrapper = mount(ObjectForm)
-    expect(wrapper.is('form')).toEqual(true)
-    expect(wrapper.contains('input')).toEqual(true)
-    expect(wrapper.contains('textarea')).toEqual(true)
+    expect(wrapper.contains('[label=Title]')).toEqual(true)
+    expect(wrapper.contains('[label=Description]')).toEqual(true)
   })
   it('has submitted method', () => {
     expect(typeof ObjectForm.methods.submit).toEqual('function')
   })
   it('clears state after submit', () => {
     const wrapper = mount(ObjectForm, {store})
-    const button = wrapper.find('button')
+    const button = wrapper.find('[name="submit"]')
     wrapper.setData({ title: 'Hello', description: 'World' })
     button.trigger('click')
     expect(wrapper.vm.title).toEqual('')
@@ -24,7 +23,7 @@ describe('Form.vue', () => {
   })
   it('does not clear when no title', () => {
     const wrapper = mount(ObjectForm)
-    const button = wrapper.find('button')
+    const button = wrapper.find('[name=submit]')
     wrapper.setData({ title: '', description: 'World' })
     button.trigger('click')
     expect(wrapper.vm.title).toEqual('')
