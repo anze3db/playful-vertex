2665ba9457d7aff01db4fcab79236dfaced9c142
Anže Pečar <anze@pecar.me>
2018-01-03 01:26:20 +0000

    Make ObjectList spec element independent

--- frontend/test/unit/specs/components/ObjectList.spec.js
+++ frontend/test/unit/specs/components/ObjectList.spec.js
@@ -4,18 +4,15 @@ import ObjectList from '@/components/ObjectList'
 import store from '@/store'
 
 describe('ObjectList.vue', () => {
-  it('should render correct contents', () => {
-    const wrapper = mount(ObjectList, {store})
-    expect(wrapper.contains('v-list')).toEqual(true)
-  })
   it('lists all the items in the store', () => {
     store.commit('add_object', {content: 'first'})
     store.commit('add_object', {content: 'second'})
     const wrapper = mount(ObjectList, {store})
-    expect(wrapper.contains('v-list')).toEqual(true)
-    const items = wrapper.findAll('v-list-tile')
+    const items = wrapper.findAll('[label=content]')
     expect(items.length).toEqual(2)
-    expect(items.at(0).find('v-list-tile-title').text()).toEqual('first')
-    expect(items.at(1).find('v-list-tile-title').text()).toEqual('second')
+    const results = ['first', 'second']
+    for (let i in results) {
+      expect(items.at(i).text()).toEqual(results[i])
+    }
   })
 })
