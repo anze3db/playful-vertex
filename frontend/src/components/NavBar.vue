294982ac2c32a7a8f0c3b3b5faad05ba78cd0b8a
Anže Pečar <anze@pecar.me>
2018-02-19 17:24:28 +0000

    Add link to navbar

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,6 +1,8 @@
 <template>
   <v-toolbar dark color="primary">
-    <v-toolbar-title class="white--text" name="title">Playful Vertex</v-toolbar-title>
+    <v-toolbar-title class="white--text" name="title">
+      <router-link to="/">Playful Vertex</router-link>
+    </v-toolbar-title>
   </v-toolbar>
 </template>
 
@@ -9,3 +11,10 @@ export default {
   name: 'NavBar'
 }
 </script>
+
+<style scoped>
+a {
+  color: white;
+  text-decoration: none;
+}
+</style>
